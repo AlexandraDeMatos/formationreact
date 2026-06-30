78a35768aa4fd709d7894445374d5d019c129dd6
Alexandra RAMPAZZI <alexandra.rampazzi@ca-cheques.credit-agricole.fr>
2026-06-30 14:05:47 +0200

    Navbar

--- src/components/uis/Navbar/Navbar.tsx
+++ src/components/uis/Navbar/Navbar.tsx
@@ -1,11 +1,23 @@
 import type { FC } from 'react';
 import styles from './Navbar.module.css';
+import {Container, Nav, Navbar as NavBar} from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => (
   <div className={styles.Navbar} data-testid="Navbar">
-    Navbar Component
+    <NavBar bg="dark" data-bs-theme="dark">
+      <Container>
+        <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </NavBar>
   </div>
 );
 
